b5d81c9f2285e87b0cb71f8307a265aa88697c9e
Olexandr Pascal <futupas@gmail.com>
2021-07-13 13:05:06 +0300

    connecting to database (mongoDB works)

---
+++ KMATutorBot/KMATutorBot/Application/Application.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMATutorBot
+{
+    internal class Application
+    {
+        public Database DB { get; private set; }
+
+        public Application()
+        {
+            try
+            {
+                DB = new();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error initiaizing Database: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ KMATutorBot/KMATutorBot/Database/Database.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace KMATutorBot
+{
+    internal class Database
+    {
+        private const string CONNECTION_STRING = @"mongodb+srv://user1:user1user1@cluster0.di7oe.mongodb.net/test";
+        public Database()
+        {
+            MongoClient client = new MongoClient(CONNECTION_STRING);
+            //IMongoDatabase database = client.GetDatabase("MainDB");
+            var dbs = client.ListDatabases();
+            var a = dbs.ToList();
+            Console.WriteLine("d");
+        }
+    }
+}
